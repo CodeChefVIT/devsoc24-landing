c9e6712356bb12dc51228efe6a806484e58a1aaf
NishantGupt786 <nishantgupta2325@gmail.com>
2024-02-21 23:11:27 +0530

    feat:added base for hero section

---
+++ devsoc24-landing/src/components/main.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import logo from "@/assets/images/logo.svg"
+import glitch from "@/assets/images/startglitch.svg"
+import title from "@/assets/images/title.svg"
+import type { StaticImport } from "next/dist/shared/lib/get-img-props";
+
+function Main() {
+  const [className, setClassName] = useState("relative min-h-screen overlaymain font-disket overflow-hidden");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setClassName("relative min-h-screen min-w-screen overlay_hero font-disket overflow-hidden");
+    }, 400);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+return (
+    <>
+        <div className={className}>
+            <Image src={title as StaticImport} alt="title" className="absolute top-4 left-1/3" />
+            <Image src={glitch as StaticImport} alt="glitch" layout="fill" />
+            <Image src={logo as StaticImport} alt="logo" width={800} className="absolute left-[18%] bottom-0" />
+        </div>
+    </>
+);
+}
+
+export default Main;
