23fe93b7675755ee94054e4fe9ff0f9963f56e07
Rajiv Dalal <rajiv.dalal2022@vitstudent.ac.in>
2024-02-22 16:21:08 +0530

    Navbar component+Hover Animation

--- devsoc24-landing/src/pages/index.tsx
+++ devsoc24-landing/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import About from "@/components/about";
 import Main from "@/components/main";
+import NavButton from "@/components/navButton";
 
 export default function Home() {
   return (
     <>
+      <NavButton link={"/"} name={"ABC"}/>
       <Main />
       <About />
     </>
