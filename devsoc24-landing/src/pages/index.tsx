8c3b8cf53732d0354a007f37de17c63086ea75c3
Abh1noob <abhinavganeshank@gmail.com>
2024-02-26 02:09:46 +0530

    fix: warning fix

--- devsoc24-landing/src/pages/index.tsx
+++ devsoc24-landing/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Timeline from "@/components/Timeline";
 import About from "@/components/about";
-import Footer from "@/components/footer";
 import Main from "@/components/main";
 import SmoothScrolling from "@/components/smoothscrolling";
 
