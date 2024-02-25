eb0731e16a6135c9bbc390933de5572495dd2b2b
NishantGupt786 <nishantgupta2325@gmail.com>
2024-02-26 03:02:19 +0530

    responsiveness

--- devsoc24-landing/src/components/screen.tsx
+++ devsoc24-landing/src/components/screen.tsx
@@ -8,7 +8,7 @@ const Screen = (props:{text:string}) => {
         <>
         <div>
             <Image src={screen as StaticImageData} alt="screen" className=" z-1"/>
-            <div className={`font-wreckside text-5xl sm:text-6xl md:text-8xl w-54 h-54 -rotate-[5deg] absolute top-[32%] sm:top-[25%] left-[43%] 2xl:left-[44%]  z-2`}>
+            <div className={`font-wreckside text-5xl sm:text-6xl md:text-8xl w-54 h-54 -rotate-[5deg] absolute top-[34%] sm:top-[25%] left-[42%] sm:left-[43%] 2xl:left-[44%]  z-2`}>
                 <p className={`tracking-wide text-center w-min ${styles.flicker}`}>{props.text}</p>
             </div>
         </div>
