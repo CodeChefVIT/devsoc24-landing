3fe8a08ac458deb5f9d517cdd5a0d4aef629d57d
Abh1noob <abhinavganeshank@gmail.com>
2024-02-28 15:01:56 +0530

    feat: made window draggable

--- devsoc24-landing/src/components/terminal/prizepool.tsx
+++ devsoc24-landing/src/components/terminal/prizepool.tsx
@@ -1,48 +1,24 @@
-"use client";
-import { useState } from "react";
+import React, { useState } from "react";
+import Image from "next/image";
 import { GiMoneyStack } from "react-icons/gi";
-// import Image from "next/image";
-// import Card from "../../components/card";
-// import Terminal from "../../components/terminal/page";
 import { MdMinimize } from "react-icons/md";
 import { IoMdClose } from "react-icons/io";
 import { BiWindow } from "react-icons/bi";
-import Image from "next/image";
+import Draggable from "react-draggable";
 import { useCloseStore } from "@/store/store";
 
 export default function Prizepool() {
   const [minimized, setMinimized] = useState(false);
   const [maximized, setMaximized] = useState(false);
-  const { activeCard, setActiveCard } = useCloseStore();
+  const { setActiveCard } = useCloseStore();
 
   const handleMinimize = () => {
     setMinimized(!minimized);
   };
+
   const handleMaximize = () => {
     setMaximized(!maximized);
   };
-
-  const cardTypes = [
-    "About",
-    "Timeline",
-    "Tracks",
-    "Prizepool",
-    "Sponsors",
-    "Portal",
-    "FAQs",
-  ];
-  const cardImage = [
-    "personabout 2.svg",
-    "Frame 13.svg",
-    "Frame 13 2.svg",
-    "Frame 13 3.svg",
-    "Frame 13 4.svg",
-    "Frame 13 5.svg",
-    "Frame 13 6.svg",
-  ];
-
-  // const [activeCard, setActiveCard] = useState<string[]>(["DEVSOC 2024"]);
-  // const { activeCard, setActiveCard } = useCloseStore();
 
   const useUserModal = () => {
     const [showUserModal, setshowUserModal] = useState(false);
@@ -58,26 +34,16 @@ export default function Prizepool() {
     return { showUserModal, openUserModal, closeUserModal };
   };
 
-  const { showUserModal, openUserModal, closeUserModal } = useUserModal();
-  // const handleClick = (cardName: string) => {
-  //   setActiveCard((currentActiveCard) => {
-  //     if (currentActiveCard.includes(cardName)) {
-  //       console.log("activecard", activeCard);
-  //       return currentActiveCard.filter((card) => card !== cardName);
-  //     } else {
-  //       console.log(activeCard);
-  //       return [...currentActiveCard, cardName];
-  //     }
-  //   });
-  // };
+  const { openUserModal } = useUserModal();
+
   return (
     <div
       className={`relative flex h-full w-full ${maximized ? "" : "pl-[30px] pt-[100px]  md:pl-[60px]"}`}
     >
-      <div className="h-[20%] bg-slate-700 ">
+      <div>
         {minimized ? (
           <div
-            className={`absolute flex h-[54px] w-[54px] items-center justify-center rounded-full border-2 border-[#a4a3a3] bg-[#757575]  hover:cursor-pointer`}
+            className="absolute flex h-[54px] w-[54px] items-center justify-center rounded-full border-2 border-[#a4a3a3] bg-[#757575]  hover:cursor-pointer"
             onClick={() => handleMinimize()}
           >
             <Image
@@ -89,15 +55,15 @@ export default function Prizepool() {
             />
           </div>
         ) : (
-          <>
+          <Draggable handle=".drag-handle">
             <div
-              className={` min-w-[300px] ${maximized ? "h-[90vh] w-[80vw]" : "h-fit w-[30vw] pb-10"} border-2 bg-[#b2b2b2] `}
+              className={`min-w-[300px] flex-grow ${maximized ? "h-[90vh]" : "h-fit w-[30vw] pb-10"} border-2 bg-[#b2b2b2]`}
             >
-              <div className="flex h-[25px] w-[100%] items-center justify-between border-b-2 bg-gradient-to-r from-blue-800 to-blue-600">
+              <div className="drag-handle flex h-[25px] w-[100%] items-center justify-between border-b-2 bg-gradient-to-r from-blue-800 to-blue-600">
                 <span className="flex items-center pl-4 text-xs text-white">
                   <GiMoneyStack className="pr-2 text-2xl" /> Pricepool
                 </span>
-                <section className="flex ">
+                <section className="flex">
                   <span
                     className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#757575] hover:cursor-pointer"
                     onClick={() => handleMinimize()}
@@ -118,6 +84,7 @@ export default function Prizepool() {
                   </span>
                 </section>
               </div>
+
               <p className="w-[90%] pl-6 pt-6 ">Coming soon</p>
               <button
                 className={`absolute bottom-2 right-4 h-6 w-24 border-b-[3px] border-r-[3px] border-[#000000] bg-[#aaa9a9] text-sm transition ease-in-out  hover:h-[26px] hover:w-[98px] hover:duration-75 md:mt-[100px]`}
@@ -126,7 +93,7 @@ export default function Prizepool() {
                 Select
               </button>
             </div>
-          </>
+          </Draggable>
         )}
       </div>
     </div>
