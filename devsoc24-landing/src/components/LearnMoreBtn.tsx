97138e73e16e938f3c2f6893caa2bdbd436445f3
Abh1noob <abhinavganeshank@gmail.com>
2024-02-29 17:35:22 +0530

    docs: added faqs

--- devsoc24-landing/src/components/LearnMoreBtn.tsx
+++ devsoc24-landing/src/components/LearnMoreBtn.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { motion } from "framer-motion";
 import styles from "@/styles/LearnMoreBtn.module.css";
+import useGlitchStore from "@/store/store";
 
 const LearnMoreBtn = (props: { link: string }) => {
+  const { showGlitch, setGlitch } = useGlitchStore();
   return (
     <>
       <motion.a
         href={props.link}
-        className={`${styles.btn} font-disket px-3 py-1 text-xl tracking-tighter`}
+        className={`${styles.btn} font-disket px-3 py-1 text-xl tracking-tighter z-50`}
         whileHover={{ backgroundPosition: "-100%,100%" }}
       >
         Learn More
