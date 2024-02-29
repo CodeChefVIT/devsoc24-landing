2830d760e2b7bf1731ba151f5ba7268beab7286e
NishantGupt786 <nishantgupta2325@gmail.com>
2024-02-29 17:46:44 +0530

    hahaha

--- devsoc24-landing/src/components/terminal/tracks.tsx
+++ devsoc24-landing/src/components/terminal/tracks.tsx
@@ -1,24 +1,33 @@
 import React, { useState } from "react";
 import Image from "next/image";
-import { GiMoneyStack } from "react-icons/gi";
-import { MdMinimize } from "react-icons/md";
-import { IoMdClose } from "react-icons/io";
-import { BiWindow } from "react-icons/bi";
-import Draggable from "react-draggable";
+import tracking from "@/assets/images/tracklogo.png";
 import { useCloseStore, useSelectedStore } from "@/store/store";
 
 export default function Tracks() {
   const [minimized, setMinimized] = useState(false);
   const [maximized, setMaximized] = useState(false);
   const { activeCard, setActiveCard } = useCloseStore();
-  const {selectedComponent, setSelectedComponent} = useSelectedStore();
+  const { selectedComponent, setSelectedComponent } = useSelectedStore();
 
+  const tracks = [
+    "AR/VR",
+    "Blockchain",
+    "Automation tools",
+    "Social good",
+    "Health and wellness",
+    "Open innovation",
+  ];
 
   return (
-    <div
-      className={`relative flex h-full w-full`}
-    >
-      
+    <div className={`flex h-full w-full`}>
+      <div className="flex flex-wrap justify-center items-center text-white my-auto">
+        {tracks.map((track, index) => (
+          <div key={index} className="flex flex-col items-center m-4 w-[300px] text-center self-start">
+            <Image src={tracking} alt="track" quality={100} />
+            <p className="font-vcr text-3xl mt-2">{track}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
