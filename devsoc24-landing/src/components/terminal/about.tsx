2d5992121a46c15b9f0830c02f3fe1875d4acc29
Rajiv Dalal <rajiv.dalal2022@vitstudent.ac.in>
2024-02-29 14:27:11 +0530

    Design Fixes in About Page

--- devsoc24-landing/src/components/terminal/about.tsx
+++ devsoc24-landing/src/components/terminal/about.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Image from "next/image";
-import { GiMoneyStack } from "react-icons/gi";
+import notepad from "@/assets/images/notepad.svg"
 import { MdMinimize } from "react-icons/md";
 import { IoMdClose } from "react-icons/io";
 import { BiWindow } from "react-icons/bi";
@@ -44,7 +44,7 @@ export default function About() {
       <div>
         {minimized ? (
           <div
-            className="absolute flex h-[54px] w-[54px] items-center justify-center rounded-full border-2 border-[#a4a3a3] bg-[#757575]  hover:cursor-pointer"
+            className="absolute flex h-[54px] w-[54px] items-center justify-center rounded-full border-2 border-[#a4a3a3] bg-transparent  hover:cursor-pointer"
             onClick={() => handleMinimize()}
           >
             <Image
@@ -61,24 +61,25 @@ export default function About() {
               className={`flex min-w-[300px] flex-grow flex-col ${maximized ? "h-[90vh]" : "h-fit w-[30vw] pb-10"} border-2 bg-[#b2b2b2]`}
             >
               <div className="drag-handle flex h-[25px] w-[100%] items-center justify-between border-b-2 bg-gradient-to-r from-blue-800 to-blue-600">
-                <span className="flex items-center pl-4 text-xs text-white">
-                  <GiMoneyStack className="pr-2 text-2xl" /> About
+                <span className="flex items-center pl-2 font-nokia text-[10px] text-white">
+                  <Image src={notepad as HTMLImageElement} alt="Notepad" className="inline mr-1"/>
+                  About
                 </span>
                 <section className="flex">
                   <span
-                    className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#757575] hover:cursor-pointer"
+                    className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:bg-[#757575] hover:cursor-pointer"
                     onClick={() => handleMinimize()}
                   >
                     <MdMinimize />
                   </span>
                   <span
-                    className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#757575] hover:cursor-pointer"
+                    className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:bg-[#757575] hover:cursor-pointer"
                     onClick={() => handleMaximize()}
                   >
                     <BiWindow />
                   </span>
                   <span
-                    className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#757575] hover:cursor-pointer"
+                    className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:bg-[#757575] hover:cursor-pointer"
                     onClick={() => {
                       setActiveCard(activeCard.filter((c) => c !== "About"));
                       setSelectedComponent(null);
@@ -89,7 +90,7 @@ export default function About() {
                 </section>
               </div>
 
-              <p className=" w-[90%] pl-6 pt-2 md:pt-8">
+              <p className="font-vcr w-[90%] pl-6 pt-2 md:pt-8">
                 DEVSOC is CodeChef-VIT’s annual flagship event hoping to empower
                 young minds by bringing enthusiastic technocrats and thinkers
                 under one roof. This year, the second iteration of the hackathon
@@ -99,19 +100,19 @@ export default function About() {
                 spread over three days, with the hackathon lasting 48 hours
                 consisting of amazing swags, prizes, food and drinks!
               </p>
-              <p className=" w-[90%] pl-6 pt-1 md:pt-4">
+              <p className="font-vcr w-[90%] pl-6 pt-1 md:pt-4">
                 Devsoc Is going to be held at Anna Auditorium, Vellore Institute
                 of Technology, Vellore
               </p>
-              <p className=" w-[90%] pl-6 pt-1 md:pt-4">
+              <p className="font-vcr w-[90%] pl-6 pt-1 md:pt-4">
                 Devsoc is going to be held on the 15th and 16th of March 2024
               </p>
-              <button
+              {/* <button
                 className={`absolute bottom-2 right-4 h-6 w-24 border-b-[3px] border-r-[3px] border-[#000000] bg-[#aaa9a9] text-sm transition ease-in-out  hover:h-[26px] hover:w-[98px] hover:duration-75 md:mt-[100px]`}
                 // onClick={openUserModal}
               >
                 Select
-              </button>
+              </button> */}
             </div>
           </Draggable>
         )}
