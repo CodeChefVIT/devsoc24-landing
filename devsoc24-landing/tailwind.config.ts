5f2737c3df900ab514d25642bb8a1ddb23e46df7
Aaditya Mahanta <aadityamahanta07@gmail.com>
2024-03-02 22:25:23 +0530

    init app

--- devsoc24-landing/tailwind.config.ts
+++ devsoc24-landing/tailwind.config.ts
@@ -7,23 +7,6 @@ export default {
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
-        'diatype': ['ABC Diatype Mono', 'monospace'], 
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      
-      
-      keyframes: {
-        slideIn: {
-          '0%': { transform: 'translateX(-100%)' },
-          '100%': { transform: 'translateX(0)' },
-        },
-      },
-      animation: {
-        slideIn: 'slideIn 2s forwards',
       },
     },
   },
