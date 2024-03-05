a08e4bdf8220b51279916b6ce84b730162b9e4dd
Abh1noob <abhinavganeshank@gmail.com>
2024-03-05 16:13:34 +0530

    fix: build

--- devsoc24-landing/src/app/loading.tsx
+++ devsoc24-landing/src/app/loading.tsx
@@ -7,13 +7,11 @@ const help = [
   "<span>Retriving command data...</span>",
   "<br>",
 ];
-const handleTypingComplete = () => {};
 const Loading: React.FC = () => {
   return (
     <div className="font-diatype h-screen w-screen text-sm font-thin text-[#757575] pl-3">
       <TypewriterEffect
         textLines={help}
-        onTypingComplete={handleTypingComplete}
       />
       {/* <div className="h-20 w-20 animate-spin rounded-full border-b-2 border-t-2 border-gray-900"></div> */}
     </div>
