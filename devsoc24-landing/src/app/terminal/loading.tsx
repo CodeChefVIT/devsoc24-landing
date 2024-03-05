515e8f721583fe7cd42defa33d7c3f9e2e6c3c9f
Abh1noob <abhinavganeshank@gmail.com>
2024-03-05 15:57:51 +0530

    feat: actual loader

--- devsoc24-landing/src/app/terminal/loading.tsx
+++ devsoc24-landing/src/app/terminal/loading.tsx
@@ -1,42 +1,21 @@
+"use client"
 import TypewriterEffect from "@/components/terminal/TypewriterEffect";
 import React from "react";
 const help = [
-  '<span class="">User validated and online...</span>',
-  '<span class="">[[init]]</span>',
-  '<span class="">Retriving command data...</span>',
-  '<span class="">[complete]</span>',
-  '<span class="">User ip found and indexed:</span>',
-  '<span class="">Initiating quantum decryption sequence</span>',
-  '<span class="">Syncing neural network interfaces.........</span>',
-  '<span class="">Overriding mainframe security protocols</span>',
-  '<span class="">Hacking into the mainframe matrix...</span>',
-  '<span class="">$User id set to 8y14e9f8</span>',
-  '<span class="">Initiating quantum decryption sequence...</span>',
-  '<span class="">Syncing neural network interfaces...</span>',
-  '<span class="">Overriding mainframe security protocols...</span>',
-  '<span class="">Hacking into the mainframe matrix...</span>',
-  '<span class="" font-weight: bold;>----------------------------------------</span>',
-
-  "<p class='block-text' style='font-size:7.4px;font-weight: bold;'>&nbsp;*******&nbsp;&nbsp;&nbsp;********&nbsp;**&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**&nbsp;&nbsp;********&nbsp;&nbsp;&nbsp;*******&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;******&nbsp;</p>",
-  "<p class='block-text' style='font-size:7.4px;font-weight: bold;'>/**////**&nbsp;/**/////&nbsp;/**&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/**&nbsp;**//////&nbsp;&nbsp;&nbsp;**/////**&nbsp;&nbsp;&nbsp;**////**</p>",
-  "<p class='block-text' style='font-size:7.4px;font-weight: bold;'>/**&nbsp;&nbsp;&nbsp;&nbsp;/**/**&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/**&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/**/**&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;//**&nbsp;**&nbsp;&nbsp;&nbsp;&nbsp;//&nbsp;</p>",
-  "<p class='block-text' style='font-size:7.4px;font-weight: bold;'>/**&nbsp;&nbsp;&nbsp;&nbsp;/**/*******&nbsp;//**&nbsp;&nbsp;&nbsp;&nbsp;**&nbsp;/*********/**&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/**/**&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>",
-  "<p class='block-text' style='font-size:7.4px;font-weight: bold;'>/**&nbsp;&nbsp;&nbsp;&nbsp;/**/**////&nbsp;&nbsp;&nbsp;//**&nbsp;&nbsp;**&nbsp;&nbsp;////////**/**&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/**/**&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>",
-  "<p class='block-text' style='font-size:7.4px;font-weight: bold;'>/**&nbsp;&nbsp;&nbsp;&nbsp;**&nbsp;/**&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;//****&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/**//**&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**&nbsp;//**&nbsp;&nbsp;&nbsp;&nbsp;**</p>",
-  "<p class='block-text' style='font-size:7.4px;font-weight: bold;'>/*******&nbsp;&nbsp;/********&nbsp;&nbsp;&nbsp;//**&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;********&nbsp;&nbsp;//*******&nbsp;&nbsp;&nbsp;//******&nbsp;</p>",
-  "<p class='block-text' style='font-size:7.4px;font-weight: bold;'>///////&nbsp;&nbsp;&nbsp;////////&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;//&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;////////&nbsp;&nbsp;&nbsp;&nbsp;///////&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;//////&nbsp;&nbsp;</p>",
-
-  '<span class="" font-weight: bold;>----------------------------------------</span>',
-  '<span class="">Disrupting network protocols with raw socket manipulation...</span>',
-  '<span class="">Executing stealth maneuvers with bitwise masking...</span>',
-  '<span class="">Loading Welcome............</span>',
-
+  "<span>User validated and online...</span>",
+  "<span>[[init]]</span>",
+  "<span>Retriving command data...</span>",
   "<br>",
 ];
+const handleTypingComplete = () => {};
 const Loading: React.FC = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="h-20 w-20 animate-spin rounded-full border-b-2 border-t-2 border-gray-900"></div>
+    <div className="font-diatype h-screen w-screen text-sm font-thin text-[#757575] pl-3">
+      <TypewriterEffect
+        textLines={help}
+        onTypingComplete={handleTypingComplete}
+      />
+      {/* <div className="h-20 w-20 animate-spin rounded-full border-b-2 border-t-2 border-gray-900"></div> */}
     </div>
   );
 };
