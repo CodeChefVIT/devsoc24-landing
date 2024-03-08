2d22e744a9a85387d6281e2e360fe38290b858f5
Abh1noob <abhinavganeshank@gmail.com>
2024-03-08 13:04:43 +0530

    ui changes

--- devsoc24-landing/src/app/layout.tsx
+++ devsoc24-landing/src/app/layout.tsx
@@ -12,6 +12,10 @@ export const metadata = {
   description:
     "DEVSOC'24 is CodeChef-VIT's annual flagship event hoping to empower young minds by bringing enthusiastic technocrats and thinkers under one roof.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  ogImage: {
+    url: "https://example.com/path/to/your/image.jpg",
+    alt: "DEVSOC'24 Image",
+  },
 };
 
 export default function RootLayout({
