0e7efe72fa984bb35a669a08f799eb9090944aa1
Abh1noob <abhinavganeshank@gmail.com>
2024-03-14 23:03:07 +0530

    final try og

--- devsoc24-landing/src/app/layout.tsx
+++ devsoc24-landing/src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           }
         />
         <meta property="og:image:type" content="image/png" />
-        <meta property="og:image" content={"/thumbnail.png"} />
+        <meta property="og:image" content="https://i.imgur.com/50QRAQ9.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:image:alt" content="DEVSOC'24" />
