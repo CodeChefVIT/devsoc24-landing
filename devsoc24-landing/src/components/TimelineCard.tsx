6b94ca5ae6c226fabbdfbfdbb515c9992b701f8f
Abh1noob <abhinavganeshank@gmail.com>
2024-03-15 23:39:23 +0530

    lmao

--- devsoc24-landing/src/components/TimelineCard.tsx
+++ devsoc24-landing/src/components/TimelineCard.tsx
@@ -1,9 +1,45 @@
 import React from "react";
+import Image from "next/image";
 
-const TimelineCard = () => {
+const TimelineCard = (props: {
+  day: number;
+  title: string;
+  time: string;
+  image: string;
+}) => {
   return (
-    <div className="bgImg">
-      <div></div>
+    <div className="min-w-[15vw] max-w-[15vw]">
+      <div className="flex flex-col">
+        <div className="relative flex h-fit w-fit items-center justify-center bg-black">
+          <Image
+            src="/timelineCard.svg"
+            alt="header"
+            height={0}
+            width={0}
+            className="h-auto min-w-[15vw] max-w-[15vw] w-full"
+          />
+          <div className="absolute h-fit w-fit bg-[#757575] p-1">
+            Day {props.day}
+          </div>
+        </div>
+        <div className="flex h-[30vh] w-full flex-col bg-[#757575]">
+          <div className="flex h-[60%] w-full items-center justify-center">
+            <Image
+              src={`/timeline/${props.image}`}
+              alt=""
+              height={0}
+              width={0}
+              className="h-full w-full p-5"
+            />
+          </div>
+          <div className="flex h-[40%] w-full flex-col items-center justify-center gap-4  text-center">
+            <p className="w-full">[{props.title}]</p>
+            <p className="w-[95%] bg-black px-2 text-white">
+              {"<"} {props.time} {">"}{" "}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- devsoc24-landing/src/components/terminal/Pages/Timeline.tsx
+++ devsoc24-landing/src/components/terminal/Pages/Timeline.tsx
@@ -1,152 +1,95 @@
 import React, { useState } from "react";
-import Image from "next/image";
-import notepad from "@/assets/images/notepad.svg";
-import { MdMinimize } from "react-icons/md";
-import { IoMdClose } from "react-icons/io";
-import { BiWindow } from "react-icons/bi";
-import Draggable from "react-draggable";
-import { useCloseStore, useSelectedStore } from "@/store/store";
+import TimelineCard from "@/components/TimelineCard";
+
+const timelineData = [
+  {
+    day: 1,
+    title: "Entry and Registration",
+    time: "18:00",
+    image: "windows.svg",
+  },
+  {
+    day: 1,
+    title: "Hacking Starts",
+    time: "20:00",
+    image: "windows.svg",
+  },
+  {
+    day: 2,
+    title: "Review 1",
+    time: "3:00",
+    image: "address.svg",
+  },
+  {
+    day: 2,
+    title: "ContentStack speaker session",
+    time: "12:00",
+    image: "microphone.svg",
+  },
+  {
+    day: 2,
+    title: "Akhil Sharma speaker session",
+    time: "16:00",
+    image: "microphone.svg",
+  },
+  {
+    day: 2,
+    title: "Saurabh Soni speaker session",
+    time: "22:00",
+    image: "microphone.svg",
+  },
+  // {
+  //   day: 3,
+  //   title: "Fun activity/competition (tbd)",
+  //   time: "1:00",
+  //   image: "",
+  // },
+  {
+    day: 3,
+    title: "Review 2",
+    time: "2:00",
+    image: "address.svg",
+  },
+  // {
+  //   day: 3,
+  //   title: "Fun activity/competition (tbd)",
+  //   time: "12:00",
+  //   image: "",
+  // },
+  {
+    day: 3,
+    title: "End of Hacking",
+    time: "13:00",
+    image: "winner.svg",
+  },
+  {
+    day: 3,
+    title: "Final Pitches",
+    time: "14:00",
+    image: "winner.svg",
+  },
+  {
+    day: 3,
+    title: "Prize distribution and Closing",
+    time: "16:00",
+    image: "winner.svg",
+  },
+];
 
 export default function Timeline() {
-  const [minimized, setMinimized] = useState(false);
-  const [maximized, setMaximized] = useState(false);
-  const { activeCard, setActiveCard } = useCloseStore();
-  const { setSelectedComponent } = useSelectedStore();
-
-  const handleMinimize = () => {
-    setMinimized(!minimized);
-  };
-
-  const handleMaximize = () => {
-    setMaximized(!maximized);
-  };
-
   return (
     <div
-      className={`relative flex h-full min-h-[92vh] w-full ${maximized ? "" : "pl-[30px] pt-[100px]  md:pl-[60px] bgImg"}`}
+      className={`bgImg md:pl-[60px]"} relative flex h-full min-h-[92vh] w-full flex-wrap gap-6 pl-[30px] pt-[100px]`}
     >
-      <div>
-        {minimized ? (
-          <div
-            className="absolute flex h-[54px] w-[54px] items-center justify-center rounded-full border-2 border-[#a4a3a3] bg-transparent  hover:cursor-pointer"
-            onClick={() => handleMinimize()}
-          >
-            <Image
-              src="Icon-notepad.svg"
-              alt="Description of the image"
-              width={40}
-              height={100}
-              className=""
-            />
-          </div>
-        ) : !maximized ? (
-          <Draggable bounds=".boundarybox">
-            <div
-              className={`absolute z-50 flex h-fit min-w-[300px] flex-grow flex-col ${maximized ? "h-[100vh] w-[100vw] sm:h-full sm:w-full" : "w-[30vw]"} border-2 bg-[#b2b2b2] pb-10`}
-            >
-              <div className="drag-handle flex h-[25px] w-[100%] items-center justify-between border-b-2 bg-gradient-to-r from-blue-800 to-blue-600">
-                <span className="font-nokia flex items-center justify-center pl-2 text-[10px] text-white">
-                  <Image
-                    src={notepad as HTMLImageElement}
-                    alt="Notepad"
-                    className="inline-block w-6 pr-3"
-                  />
-                  Timeline
-                </span>
-                <section className="flex">
-                  <span
-                    className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
-                    onClick={() => setSelectedComponent("DEVSOC 2024")}
-                    onTouchEnd={() => setSelectedComponent("DEVSOC 2024")}
-                  >
-                    <MdMinimize />
-                  </span>
-                  <span
-                    className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
-                    onClick={() => handleMaximize()}
-                    onTouchEnd={() => handleMaximize()}
-                  >
-                    <BiWindow />
-                  </span>
-                  <span
-                    className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
-                    onClick={() => {
-                      setActiveCard(activeCard.filter((c) => c !== "Timeline"));
-                      setSelectedComponent("DEVSOC 2024");
-                    }}
-                    onTouchEnd={() => {
-                      setActiveCard(activeCard.filter((c) => c !== "Timeline"));
-                      setSelectedComponent("DEVSOC 2024");
-                    }}
-                  >
-                    <IoMdClose />
-                  </span>
-                </section>
-              </div>
-              <p className="w-[90%] pl-6 pt-6 ">17th March to 19th March</p>
-              <button
-                //hover: className={`absolute self-end h-6 w-24 border-b-[3px] border-r-[3px] border-[#000000] bg-[#aaa9a9] text-sm transition ease-in-out  hover:scale-[1.05] hover:duration-75 md:mb-2 md:mr-2 bottom-0`}
-                disabled={true}
-                className={`absolute bottom-0 h-6 w-24 self-end border-b-[3px] border-r-[3px] border-[#000000]/70 bg-[#aaa9a9]/70 text-sm text-black/70 transition ease-in-out md:mb-2 md:mr-2`}
-              >
-                Select
-              </button>
-            </div>
-          </Draggable>
-        ) : (
-          <div
-            className={`absolute z-50 flex h-fit min-w-[300px] flex-grow flex-col ${maximized ? "h-[100vh] w-[100vw] sm:h-full sm:w-full" : "w-[30vw]"} border-2 bg-[#b2b2b2] pb-10`}
-          >
-            <div className="drag-handle flex h-[25px] w-[100%] items-center justify-between border-b-2 bg-gradient-to-r from-blue-800 to-blue-600">
-              <span className="font-nokia flex items-center justify-center pl-2 text-[10px] text-white">
-                <Image
-                  src={notepad as HTMLImageElement}
-                  alt="Notepad"
-                  className="inline-block w-6 pr-3"
-                />
-                Timeline
-              </span>
-              <section className="flex">
-                <span
-                  className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
-                  onClick={() => setSelectedComponent("DEVSOC 2024")}
-                  onTouchEnd={() => setSelectedComponent("DEVSOC 2024")}
-                >
-                  <MdMinimize />
-                </span>
-                <span
-                  className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
-                  onClick={() => handleMaximize()}
-                  onTouchEnd={() => handleMaximize()}
-                >
-                  <BiWindow />
-                </span>
-                <span
-                  className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
-                  onClick={() => {
-                    setActiveCard(activeCard.filter((c) => c !== "Timeline"));
-                    setSelectedComponent("DEVSOC 2024");
-                  }}
-                  onTouchEnd={() => {
-                    setActiveCard(activeCard.filter((c) => c !== "Timeline"));
-                    setSelectedComponent("DEVSOC 2024");
-                  }}
-                >
-                  <IoMdClose />
-                </span>
-              </section>
-            </div>
-            <p className="w-[90%] pl-6 pt-6 ">17th March - 19th March</p>
-            <button
-              //hover: className={`absolute self-end h-6 w-24 border-b-[3px] border-r-[3px] border-[#000000] bg-[#aaa9a9] text-sm transition ease-in-out  hover:scale-[1.05] hover:duration-75 md:mb-2 md:mr-2 bottom-0`}
-              disabled={true}
-              className={`absolute bottom-0 h-6 w-24 self-end border-b-[3px] border-r-[3px] border-[#000000]/70 bg-[#aaa9a9]/70 text-sm text-black/70 transition ease-in-out md:mb-2 md:mr-2`}
-            >
-              Select
-            </button>
-          </div>
-        )}
-      </div>
+      {timelineData.map((item, index) => (
+        <TimelineCard
+          key={index}
+          day={item.day}
+          title={item.title}
+          time={item.time}
+          image={item.image}
+        />
+      ))}
     </div>
   );
 }
