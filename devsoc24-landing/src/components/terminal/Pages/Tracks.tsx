2d76b1b4d831b97d5347cded506e491f65788def
NishantGupt786 <nishantgupta2325@gmail.com>
2024-03-15 17:52:07 +0530

    tracks

--- devsoc24-landing/src/components/terminal/Pages/Tracks.tsx
+++ devsoc24-landing/src/components/terminal/Pages/Tracks.tsx
@@ -1,156 +1,129 @@
 import React, { useState } from "react";
 import Image from "next/image";
-import notepad from "@/assets/images/notepad.svg";
-import { MdMinimize } from "react-icons/md";
-import { IoMdClose } from "react-icons/io";
-import { BiWindow } from "react-icons/bi";
+import tracking from "@/assets/images/tracklogo.png";
 import Draggable from "react-draggable";
-import { useCloseStore, useSelectedStore } from "@/store/store";
+import { IoMdClose } from "react-icons/io";
+import notepad from "@/assets/images/notepad.svg";
 
-export default function Tracks() {
-  const [minimized, setMinimized] = useState(false);
-  const [maximized, setMaximized] = useState(false);
-  const { activeCard, setActiveCard } = useCloseStore();
-  const { setSelectedComponent } = useSelectedStore();
+const trackDetails: Track[] = [
+  {
+    title: "Interactive Engagement (Gamified Solutions)",
+    name: "Interactive Engagement",
+    content:
+      "This track is all about engaging applications with gamified mechanisms. Create solutions which promote enhanced interaction and user-retention with the help of immersive technologies and reward systems.",
+  },
+  {
+    title: "Eco-Innovations (Sustainable Technology)",
+    name: "Eco-Innovations",
+    content:
+      "Technology is all about problem solving and enriching the life of users. Provide technical solutions which promote sustainability and a better environment. Improve the current technologies to make them more sustainable or develop brand new solutions.",
+  },
+  {
+    title: "Community Building",
+    name: "Community Building",
+    content:
+      "This track focuses on fostering stronger, more connected communities. Participants are tasked with creating platforms or tools that promote unity and communal engagement.",
+  },
+  {
+    title: "Future of Work",
+    name: "Future of Work",
+    content:
+      "This track explores the evolving nature of work in the digital era. Participants are encouraged to develop solutions that enhance productivity and redefine traditional work models. Participants can also build solutions that fine-tune LLMs or use vector databases to change the work landscape.",
+  },
+  {
+    title: "Ethical Technology",
+    name: "Ethical Technology",
+    content:
+      "This track focuses on developing solutions promoting responsible innovation, privacy, fairness, and transparency in the tech industry. Participants are tasked with addressing societal concerns such as data privacy and security. Participants can also focus on combating unethical use of tech and emphasise integrity and accountability in technology design and deployment.",
+  },
+  {
+    title: "Open Innovation",
+    name: "Open Innovation",
+    content:
+      "This track focuses on embracing creativity to solve diverse challenges. Participants are encouraged to explore groundbreaking ideas across sectors and implement interdisciplinary approaches and collaborative problem-solving. If you have any idea that doesn’t fit in any of the other tracks, this track is for you.",
+  },
+];
+interface Track {
+  title: string;
+  name: string;
+  content: React.ReactNode;
+}
 
-  const handleMinimize = () => {
-    setMinimized(!minimized);
+const Tracks = () => {
+  const [trackModal, setTrackModal] = useState<boolean[]>(
+    Array(trackDetails.length).fill(false),
+  );
+
+  const showModal = (index: number) => {
+    // console.log("Open Modal: ", index);
+    setTrackModal((prev) => {
+      const newTrackModal = [...prev];
+      newTrackModal[index] = true;
+      return newTrackModal;
+    });
   };
 
-  const handleMaximize = () => {
-    setMaximized(!maximized);
+  const closeTrackModal = (
+    index: number,
+    event?:
+      | React.MouseEvent<HTMLDivElement, MouseEvent>
+      | React.TouchEvent<HTMLDivElement>,
+  ) => {
+    if (event) {
+      event.stopPropagation();
+    }
+    setTrackModal((prev) => {
+      const newTrackModal = [...prev];
+      newTrackModal[index] = false;
+      return newTrackModal;
+    });
   };
 
   return (
-    <div
-      className={`relative flex h-full w-full ${maximized ? "" : "pl-[30px] pt-[100px]  md:pl-[60px]"} bgImg`}
-    >
-      <div>
-        {minimized ? (
+    <div className={`bgImg boundarybox relative flex h-full w-full`}>
+      <div className="my-auto flex flex-wrap items-center justify-center text-white">
+        {trackDetails.map((track, index) => (
           <div
-            className="absolute flex h-[54px] w-[54px] items-center justify-center rounded-full border-2 border-[#a4a3a3] bg-transparent  hover:cursor-pointer"
-            onClick={() => handleMinimize()}
+            key={index}
+            className="m-4 flex w-[300px] flex-col items-center self-start text-center"
+            onClick={() => showModal(index)}
           >
-            <Image
-              src="Icon-notepad.svg"
-              alt="Description of the image"
-              width={40}
-              height={100}
-              className=""
-            />
+            <Image src={tracking} alt="track" quality={100} />
+            <p className="font-vcr mt-2 text-3xl">{track.name}</p>
+            {trackModal[index] && (
+              <Draggable bounds=".boundarybox">
+                <div
+                  className={`fixed top-16 z-50 lg:absolute ${index == 0 ? "lg:left-28" : ""}  flex h-fit w-[100vw] flex-col overflow-y-auto overflow-x-hidden border-2 bg-[#b2b2b2] pb-10 md:w-[500px] `}
+                >
+                  <div className="drag-handle flex min-h-[25px] h-fit items-center justify-between border-b-2 bg-gradient-to-r from-blue-800 to-blue-600">
+                    <span className="font-nokia flex items-center justify-center pl-2 text-[10px] text-white">
+                      <Image
+                        src={notepad as HTMLImageElement}
+                        alt="Notepad"
+                        className="inline-block w-6 pr-3"
+                      />
+                      {track.title}
+                    </span>
+
+                    <div
+                      className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
+                      onClick={(e) => closeTrackModal(index, e)}
+                      onTouchEnd={(e) => closeTrackModal(index, e)}
+                    >
+                      <IoMdClose />
+                    </div>
+                  </div>
+                  <div className={`h-fit w-[100vw] p-5 md:w-[500px] lg:w-fit`}>
+                    {track.content}
+                  </div>
+                </div>
+              </Draggable>
+            )}
           </div>
-        ) : !maximized ? (
-          <Draggable bounds=".boundarybox">
-            <div
-              className={`absolute z-50 flex h-fit min-w-[300px] flex-grow flex-col ${maximized ? "h-[100vh] w-[100vw] sm:h-full sm:w-full" : "w-[30vw]"} border-2 bg-[#b2b2b2] pb-10`}
-            >
-              <div className="drag-handle flex h-[25px] w-[100%] items-center justify-between border-b-2 bg-gradient-to-r from-blue-800 to-blue-600">
-                <span className="font-nokia flex items-center justify-center pl-2 text-[10px] text-white">
-                  <Image
-                    src={notepad as HTMLImageElement}
-                    alt="Notepad"
-                    className="inline-block w-6 pr-3"
-                  />
-                  Tracks
-                </span>
-                <section className="flex">
-                  <span
-                    className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
-                    onClick={() => setSelectedComponent("DEVSOC 2024")}
-                    onTouchEnd={() => setSelectedComponent("DEVSOC 2024")}
-                  >
-                    <MdMinimize />
-                  </span>
-                  <span
-                    className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
-                    onClick={() => handleMaximize()}
-                    onTouchEnd={() => handleMaximize()}
-                  >
-                    <BiWindow />
-                  </span>
-                  <span
-                    className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
-                    onClick={() => {
-                      setActiveCard(
-                        activeCard.filter((c) => c !== "Tracks"),
-                      );
-                      setSelectedComponent("DEVSOC 2024");
-                    }}
-                    onTouchEnd={() => {
-                      setActiveCard(
-                        activeCard.filter((c) => c !== "Tracks"),
-                      );
-                      setSelectedComponent("DEVSOC 2024");
-                    }}
-                  >
-                    <IoMdClose />
-                  </span>
-                </section>
-              </div>
-              <p className="w-[90%] pl-6 pt-6 ">Coming Soon...</p>
-              <button
-                //hover: className={`absolute self-end h-6 w-24 border-b-[3px] border-r-[3px] border-[#000000] bg-[#aaa9a9] text-sm transition ease-in-out  hover:scale-[1.05] hover:duration-75 md:mb-2 md:mr-2 bottom-0`}
-                disabled={true}
-                className={`absolute bottom-0 h-6 w-24 self-end border-b-[3px] border-r-[3px] border-[#000000]/70 bg-[#aaa9a9]/70 text-sm text-black/70 transition ease-in-out md:mb-2 md:mr-2`}
-              >
-                Select
-              </button>
-            </div>
-          </Draggable>
-        ) : (
-          <div
-            className={`absolute z-50 flex h-fit min-w-[300px] flex-grow flex-col ${maximized ? "h-[100vh] w-[100vw] sm:h-full sm:w-full" : "w-[30vw]"} border-2 bg-[#b2b2b2] pb-10`}
-          >
-            <div className="drag-handle flex h-[25px] w-[100%] items-center justify-between border-b-2 bg-gradient-to-r from-blue-800 to-blue-600">
-              <span className="font-nokia flex items-center justify-center pl-2 text-[10px] text-white">
-                <Image
-                  src={notepad as HTMLImageElement}
-                  alt="Notepad"
-                  className="inline-block w-6 pr-3"
-                />
-                Tracks
-              </span>
-              <section className="flex">
-                <span
-                  className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
-                  onClick={() => setSelectedComponent("DEVSOC 2024")}
-                  onTouchEnd={() => setSelectedComponent("DEVSOC 2024")}
-                >
-                  <MdMinimize />
-                </span>
-                <span
-                  className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
-                  onClick={() => handleMaximize()}
-                  onTouchEnd={() => handleMaximize()}
-                >
-                  <BiWindow />
-                </span>
-                <span
-                  className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
-                  onClick={() => {
-                    setActiveCard(activeCard.filter((c) => c !== "Tracks"));
-                    setSelectedComponent("DEVSOC 2024");
-                  }}
-                  onTouchEnd={() => {
-                    setActiveCard(activeCard.filter((c) => c !== "Tracks"));
-                    setSelectedComponent("DEVSOC 2024");
-                  }}
-                >
-                  <IoMdClose />
-                </span>
-              </section>
-            </div>
-            <p className="w-[90%] pl-6 pt-6 ">Coming soon...</p>
-            <button
-              //hover: className={`absolute self-end h-6 w-24 border-b-[3px] border-r-[3px] border-[#000000] bg-[#aaa9a9] text-sm transition ease-in-out  hover:scale-[1.05] hover:duration-75 md:mb-2 md:mr-2 bottom-0`}
-              disabled={true}
-              className={`absolute bottom-0 h-6 w-24 self-end border-b-[3px] border-r-[3px] border-[#000000]/70 bg-[#aaa9a9]/70 text-sm text-black/70 transition ease-in-out md:mb-2 md:mr-2`}
-            >
-              Select
-            </button>
-          </div>
-        )}
+        ))}
       </div>
     </div>
   );
-}
+};
+
+export default Tracks;
