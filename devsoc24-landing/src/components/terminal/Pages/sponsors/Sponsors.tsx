4813a72184c03f362f1c07433e2aecf828fb67c3
Abh1noob <abhinavganeshank@gmail.com>
2024-03-15 17:54:35 +0530

    hmmmmmmmmm

--- devsoc24-landing/src/components/terminal/Pages/sponsors/Sponsors.tsx
+++ devsoc24-landing/src/components/terminal/Pages/sponsors/Sponsors.tsx
@@ -5,7 +5,7 @@ import notepad from "@/assets/images/notepad.svg";
 import { MdMinimize } from "react-icons/md";
 import { IoMdClose } from "react-icons/io";
 import { BiWindow } from "react-icons/bi";
-import Draggable from "react-draggable";
+
 import {
   useCloseStore,
   useMobileTerminalStore,
@@ -13,6 +13,7 @@ import {
   useTerminalStore,
 } from "@/store/store";
 import SponsorCard from "./SponsorCard";
+import SponsorCardL from "./SponsorCardL";
 
 export default function Prizepool() {
   const [minimized, setMinimized] = useState(false);
@@ -52,80 +53,80 @@ export default function Prizepool() {
     <div
       className={`bgImg relative flex w-full ${showTerminal ? (!isSmaller ? "h-full " : size ? "h-[38vh] flex-col items-center gap-6 overflow-y-auto " : "h-[58vh] flex-col items-center gap-6 overflow-y-auto") : "h-full "}`}
     >
-      <Draggable bounds=".boundarybox" disabled={isSmaller}>
-        <div
-          className={`${isSmaller ? "" : "absolute left-[31%] top-16 "} z-50 flex h-fit min-w-[300px] flex-grow flex-col ${maximized ? "h-[100vh] w-[100vw] sm:h-full sm:w-full" : " w-[30vw]"} border-2 bg-[#b2b2b2] pb-3`}
-        >
-          <div className="drag-handle absolute top-0 flex h-[25px] w-[100%] items-center justify-between border-b-2 bg-gradient-to-r from-blue-800 to-blue-600">
-            <span className="font-nokia flex items-center justify-center pl-2 text-[10px] text-white">
-              <Image
-                src={notepad as HTMLImageElement}
-                alt="Notepad"
-                className="inline-block w-6 pr-3"
-              />
-              Sponsors
-            </span>
-            <section className="flex">
-              <span
-                className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
-                // onClick={() => setSelectedComponent("DEVSOC 2024")}
-                // onTouchEnd={() => setSelectedComponent("DEVSOC 2024")}
-              >
-                <MdMinimize />
-              </span>
-              <span
-                className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
-                // onClick={() => handleMaximize()}
-                // onTouchEnd={() => handleMaximize()}
-              >
-                <BiWindow />
-              </span>
-              <span
-                className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
-                onClick={() => {
-                  setActiveCard(activeCard.filter((c) => c !== "Prizepool"));
-                  setSelectedComponent("DEVSOC 2024");
-                }}
-                onTouchEnd={() => {
-                  setActiveCard(activeCard.filter((c) => c !== "Prizepool"));
-                  setSelectedComponent("DEVSOC 2024");
-                }}
-              >
-                <IoMdClose />
-              </span>
-            </section>
-          </div>
-          <div
-            className={`mt-6 flex h-fit w-full ${isSmaller ? "" : "max-w-[35vw]"} flex-col items-center justify-center`}
-          >
-            <SponsorCard
-              height={100}
-              imgsrc="/assets/sponsors/contentstack.svg"
-              title="Title Sponsor"
-              width={200}
+      <div
+        className={`${isSmaller ? "" : "absolute left-[31%] top-16 "} z-50 flex h-fit min-w-[300px]  flex-col ${maximized ? "h-[100vh] w-[100vw] sm:h-full sm:w-full" : " w-[30vw]"} border-2 bg-[#b2b2b2] pb-3`}
+      >
+        <div className="drag-handle  top-0 flex h-[25px] w-[100%] items-center justify-between border-b-2 bg-gradient-to-r from-blue-800 to-blue-600">
+          <span className="font-nokia flex items-center justify-center pl-2 text-[10px] text-white">
+            <Image
+              src={notepad as HTMLImageElement}
+              alt="Notepad"
+              className="inline-block w-6 pr-3"
             />
-            <a
-              href="https://www.contentstack.com/"
-              target="_blank"
-              rel="noreferrer noopener"
-              className="flex w-full items-center justify-center"
+            Sponsors
+          </span>
+          <section className="flex">
+            <span
+              className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
+              // onClick={() => setSelectedComponent("DEVSOC 2024")}
+              // onTouchEnd={() => setSelectedComponent("DEVSOC 2024")}
             >
-              <button
-                className={`bottom-0 mt-4 h-6 w-24 border-b-[3px] border-r-[3px] border-[#000000] bg-[#aaa9a9] text-sm transition  ease-in-out hover:scale-[1.05] hover:duration-75 md:mb-2 md:mr-2`}
-                disabled={false}
-                //className={`mt-4 h-6 w-24 border-b-[3px] border-r-[3px] border-[#000000]/70 bg-[#aaa9a9]/70 text-sm text-black/70 transition ease-in-out md:mb-2 md:mr-2`}
-              >
-                Visit
-              </button>
-            </a>
-          </div>
+              <MdMinimize />
+            </span>
+            <span
+              className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
+              // onClick={() => handleMaximize()}
+              // onTouchEnd={() => handleMaximize()}
+            >
+              <BiWindow />
+            </span>
+            <span
+              className="mr-1 border-b-[2px] border-r-[2px] border-[#1e1e1e] bg-[#b0b0b0] hover:cursor-pointer hover:bg-[#757575]"
+              onClick={() => {
+                setActiveCard(activeCard.filter((c) => c !== "Prizepool"));
+                setSelectedComponent("DEVSOC 2024");
+              }}
+              onTouchEnd={() => {
+                setActiveCard(activeCard.filter((c) => c !== "Prizepool"));
+                setSelectedComponent("DEVSOC 2024");
+              }}
+            >
+              <IoMdClose />
+            </span>
+          </section>
         </div>
-      </Draggable>
-      <Draggable bounds=".boundarybox" disabled={isSmaller}>
         <div
-          className={`${isSmaller ? "" : "absolute bottom-16 left-24"} z-50 flex h-fit min-w-[300px] flex-grow flex-col ${maximized ? "h-[100vh] w-[100vw] sm:h-full sm:w-full" : " w-[30vw]"} border-2 bg-[#b2b2b2] pb-3`}
+          className={`mt-6 flex h-fit w-full ${isSmaller ? "" : "max-w-[35vw]"} flex-col items-center justify-center`}
         >
-          <div className="drag-handle absolute top-0 flex h-[25px] w-[100%] items-center justify-between border-b-2 bg-gradient-to-r from-blue-800 to-blue-600">
+          <SponsorCard
+            height={100}
+            imgsrc="/assets/sponsors/contentstack.svg"
+            title="Title Sponsor"
+            width={200}
+          />
+          <a
+            href="https://www.contentstack.com/"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="flex w-full items-center justify-center"
+          >
+            <button
+              className={`bottom-0 mt-4 h-6 w-24 border-b-[3px] border-r-[3px] border-[#000000] bg-[#aaa9a9] text-sm transition  ease-in-out hover:scale-[1.05] hover:duration-75 md:mb-2 md:mr-2`}
+              disabled={false}
+              //className={`mt-4 h-6 w-24 border-b-[3px] border-r-[3px] border-[#000000]/70 bg-[#aaa9a9]/70 text-sm text-black/70 transition ease-in-out md:mb-2 md:mr-2`}
+            >
+              Visit
+            </button>
+          </a>
+        </div>
+      </div>
+      <div
+        className={`flex  ${isSmaller ? "flex-col" : "flex-row absolute bottom-16 w-full"}   justify-evenly`}
+      >
+        <div
+          className={`${isSmaller ? "" : ""} z-50 flex h-fit min-w-[300px]  flex-col ${maximized ? "h-[100vh] w-[100vw] sm:h-full sm:w-full" : " w-[30vw]"} border-2 bg-[#b2b2b2] pb-3`}
+        >
+          <div className="drag-handle  top-0 flex h-[25px] w-[100%] items-center justify-between border-b-2 bg-gradient-to-r from-blue-800 to-blue-600">
             <span className="font-nokia flex items-center justify-center pl-2 text-[10px] text-white">
               <Image
                 src={notepad as HTMLImageElement}
@@ -191,12 +192,11 @@ export default function Prizepool() {
             </div>
           </div>
         </div>
-      </Draggable>
-      <Draggable bounds=".boundarybox" disabled={isSmaller}>
+
         <div
-          className={`${isSmaller ? "" : "absolute bottom-16 right-11"} z-50 flex h-fit w-fit min-w-[300px] flex-grow flex-col ${maximized ? "h-[100vh] w-[100vw] sm:h-full sm:w-full" : " w-[33vw]"} border-2 bg-[#b2b2b2] pb-3`}
+          className={`${isSmaller ? "" : ""} z-50 mt-6 flex h-fit w-fit min-w-[300px] flex-col ${maximized ? "h-[100vh] w-[100vw] sm:h-full sm:w-full" : " w-[33vw]"} border-2 bg-[#b2b2b2] pb-3`}
         >
-          <div className="drag-handle absolute top-0 flex h-[25px] w-[100%] items-center justify-between border-b-2 bg-gradient-to-r from-blue-800 to-blue-600">
+          <div className="drag-handle  top-0 flex h-[25px] w-[100%] items-center justify-between border-b-2 bg-gradient-to-r from-blue-800 to-blue-600">
             <span className="font-nokia flex items-center justify-center pl-2 text-[10px] text-white">
               <Image
                 src={notepad as HTMLImageElement}
@@ -235,15 +235,16 @@ export default function Prizepool() {
               </span>
             </section>
           </div>
+
           <div
-            className={`mt-6 flex h-fit  min-w-fit ${isSmaller ? "w-fit" : " max-w-[35vw] w-fit"} flex-col justify-center gap-x-4 px-3`}
+            className={`mt-6 flex h-fit  min-w-fit ${isSmaller ? "w-full" : "  w-fit"} flex-col justify-center gap-x-4 px-3`}
           >
             <p className="mt-2 text-center text-[15px]">In-Kind Sponsors</p>
             <div
-              className={`flex w-fit ${isSmaller ? "flex-col gap-y-6" : "flex-row gap-x-4"} `}
+              className={`flex  ${isSmaller ? "flex-col gap-y-6  w-full justify-center items-center " : "flex-row gap-x-4 w-fit"} `}
             >
-              <div className={`flex w-fit flex-col`}>
-                <SponsorCard
+              <div className={`flex w-fit flex-col `}>
+                <SponsorCardL
                   height={100}
                   imgsrc="/assets/sponsors/hallofcricket.svg"
                   title=""
@@ -265,7 +266,7 @@ export default function Prizepool() {
                 </a>
               </div>
               <div className="flex w-fit flex-col">
-                <SponsorCard
+                <SponsorCardL
                   height={100}
                   imgsrc="/assets/sponsors/quillbot.svg"
                   title=""
@@ -287,7 +288,7 @@ export default function Prizepool() {
                 </a>
               </div>
               <div className="flex w-fit flex-col">
-                <SponsorCard
+                <SponsorCardL
                   height={100}
                   imgsrc="/assets/sponsors/scribbr.svg"
                   title=""
@@ -311,7 +312,7 @@ export default function Prizepool() {
             </div>
           </div>
         </div>
-      </Draggable>
+      </div>
     </div>
   );
 }
